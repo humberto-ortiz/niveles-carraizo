b67408c92ee39d1bae98b7a988cbabdd1bbb513f
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2015-06-04 21:36:31 -0400

    Fixed svg render of pygal.

--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,7 @@ import os
 import pygal
 from datetime import datetime, timedelta
 from flask import Flask, request, flash, url_for, redirect, \
-     render_template, abort, send_from_directory
+     render_template, abort, send_from_directory, make_response
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
@@ -27,12 +27,12 @@ def niveles():
 def plot():
     datey = pygal.DateY(x_label_rotation=20)
     datey.add("Visits", [
-        (datetime(2013, 1, 2), 300),
-        (datetime(2013, 1, 12), 412),
-        (datetime(2013, 2, 2), 823),
-        (datetime(2013, 2, 22), 672)
-        ])
-    datey.render()
+    (datetime(2013, 1, 2), 300),
+    (datetime(2013, 1, 12), 412),
+    (datetime(2013, 2, 2), 823),
+    (datetime(2013, 2, 22), 672)
+    ])
+    return make_response(datey.render())
 
 if __name__ == '__main__':
     app.run(app.config['IP'], app.config['PORT'])
