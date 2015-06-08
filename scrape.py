6d71e7e557bd6311bb7700f720aefa9f5763e670
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2015-06-08 11:45:41 -0400

    AAA moved page with results, changed format.

--- scrape.py
+++ scrape.py
@@ -1,14 +1,19 @@
-# http://docs.python-guide.org/en/latest/scenarios/scrape/
+# See http://docs.python-guide.org/en/latest/scenarios/scrape/
+# for a tutorial on scraping html
 
 from lxml import html
 import requests
 import datetime
 
-page = requests.get('http://www.acueductospr.com/NIVELES_COE/niveles.html')
+page = requests.get('http://www.acueductospr.com/AAARepresas/reservoirlist')
 tree = html.fromstring(page.text)
 
-#This will create a list of embalses:
-embalses = tree.xpath('//h3/strong/text()')
+#This will create a list of reservoir levels:
+embalses = tree.xpath('//div[@class="locationLevel"]/text()')
 
 today = datetime.date.today()
-print str(today), embalses[1]
+print str(today),
+for location in embalses:
+    if location.startswith("Carra"):
+        place, level = location.split()
+        print level
