312a3067a4fb06fac9a81499b65cc56490f4599c
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2015-06-26 10:27:55 -0400

    Change description and author

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='CarraizoApp',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='Use Flask and pygal to plot water levels in Carraizo',
+      author='Humberto Ortiz-Zuazaga',
+      author_email='humberto.ortiz@upr.edu',
       url='http://www.python.org/sigs/distutils-sig/',
      install_requires=['Flask>=0.10.1'],
      )
