542b36b51ec9c2183a2c03f6ffcfa9b54ef5a0de
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2015-08-24 15:20:32 -0400

    Added plot of entire dataset

--- flaskapp.py
+++ flaskapp.py
@@ -84,5 +84,28 @@ def plot():
     # pygal can render flask response directly
     return datey.render_response()
 
+@app.route("/plotall")
+def plotall():
+    niveles = []
+    with open(data_dir + "/niveles.txt","r") as f:
+        for line in f:
+            fields = line.split()
+            niveles.append( ( datetime.strptime(fields[0], "%Y-%m-%d"), float(fields[1]) ) )
+
+    start, trash = niveles[0]
+    end, trash = niveles[-1]
+    datey = pygal.DateY(x_label_rotation=20,range=(30.0,41.14),title=u'Niveles',
+    y_title="Metros sobre el nivel del mar")
+    # Can't fix label format, this doesn't work.
+    datey.x_label_format = "%Y-%m-%d"
+    datey.add("Carraizo", niveles)
+    datey.add("Optimo", [(start, 41.14), (end, 41.14)])
+    datey.add("Observacion", [(start, 39.5), (end, 39.5)])
+    datey.add("Ajustes", [(start, 38.5), (end, 38.5)])
+    datey.add("Control", [(start, 36.5), (end, 36.5)])
+    datey.add("Fuera de servicio", [(start, 30.0), (end, 30.0)])
+    # pygal can render flask response directly
+    return datey.render_response()
+
 if __name__ == '__main__':
     app.run(app.config['IP'], app.config['PORT'])
